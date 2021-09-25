097b0c8245601483a52e02737c0fb5089c14dd73
bibin20TI <bibin.aripin_ti20@nusaputra.ac.id>
2021-09-26 04:34:35 +0700

    program pertama php

--- Tugas2.php
+++ Tugas2.php
@@ -2,9 +2,27 @@
    
    class Tugas2 {
 
+      private $warna;
+      private $transmisi = "manual";
+      private $jumlahRoda;
 
-   	     
+
+      public function jalankan($warna,$jumlahRoda)
+      {
+      	$this->warna;
+      	$this->jumlahRoda;
+      	$this->transmisi;
+        
+        $message = "Mobil berwarna ".$this->warna." dengan jumlah roda ".$this->jumlahRoda." dengan transmisi ".$this->transmisi;
+      	return $message;
+      }
+
    }
+
+   $mobil = new Tugas2();
+   echo $mobil->jalankan("Merah",4);
+
+
 
 
  ?>
