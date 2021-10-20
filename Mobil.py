3063f0bb4a7b4a34083ed2ef90bd39f1e5724a4a
bibin20TI <bibin.aripin_ti20@nusaputra.ac.id>
2021-10-20 15:11:20 +0700

    menambahkan nama fungsi jalankan yang ada di method child dan overide method tersebut

--- Mobil.py
+++ Mobil.py
@@ -5,9 +5,8 @@ class Mobil():
         self.transmisi = transmisi                                                   #TUGAS OOP : inheritance(pewarisan)
         self.merk = merk  
 
-    def jalankan(self):
-        print("ayo jalan "+self.merk)	
-
+    def jalankan(self):                                          #method jalankan class parent 
+        print("ayo jalan "+self.merk)
 class Mobiltruck(Mobil):                                                                
      
     def __init__(self, jumlah_roda, transmisi, merk, muatan_max):
@@ -16,13 +15,17 @@ class Mobiltruck(Mobil):
         self.muatan = muatan_max
 
     def maxMuatan(self, muatan):
-        print("max muatan "+self.muatan)    
+        print("max muatan "+self.muatan)  
+
+    def jalankan(self):                                            #method jalankan class child
+        print("ayo jalan "+self.merk)                              #overide method jalankan pada class Mobiltruck
                                  
 sedan = Mobil(4, "auto", "mazda")
 sedan.jalankan()
 truck = Mobiltruck(8, "manual", "hino", 1000)
+truck.jalankan()
 
 print(sedan.__dict__)
 print(truck.__dict__)
- 
+  
  
