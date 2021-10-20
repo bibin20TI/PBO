f310d7bf48874164b5ccab43671b2370192a2962
bibin20TI <bibin.aripin_ti20@nusaputra.ac.id>
2021-10-20 13:27:29 +0700

    menambahkan inheritance class Mobiltruck

--- Mobil.py
+++ Mobil.py
@@ -1,14 +1,19 @@
 class Mobil():
-	    
-    def __init__(self, jumlah_roda, transmisi, warna, merk):  # Nama  : Bibin aripin
-    	self.jumlah_roda = jumlah_roda                        # Kelas : TI20C
-    	self.transmisi = transmisi                            # NIM   : 20200040002
-    	self.warna = warna                                    # Mata Kuliah : Pemograman Berorientasi Objek
-    	self.merk = merk
-    	
+
+    def __init__(self, jumlah_roda, transmisi, merk):  
+        self.jumlah_roda = jumlah_roda                        
+        self.transmisi = transmisi                            
+        self.merk = merk  
+
     def jalankan(self):
         print("ayo jalan "+self.merk)	
 
-sedan = Mobil(4, 'auto', 'putih', 'mazda')
-print(sedan.__dict__)
+class Mobiltruck(Mobil):                                                                
+     pass                                 
+   
+sedan = Mobil(4, "auto", "mazda")
+truck = Mobiltruck(8, "manual", "volvo")
 sedan.jalankan()
+truck.jalankan()
+ 
+    	
